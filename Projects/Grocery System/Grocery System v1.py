cd4f1931cf790d9045f671f3e815587a9072e674
phemelo-ds <pshuping88@gmail.com>
2026-05-05 17:55:22 +0200

    refactor: improve system structure and add input validation safeguards
    - reorganised core logic for clearer control flow
    - added error handling for user input robustness
    - reduced unnecessary user prompts to streamline interaction
    - improved overall usability and stability of CLI workflow

--- Projects/Grocery System/Grocery System v1.py
+++ Projects/Grocery System/Grocery System v1.py
@@ -10,23 +10,96 @@ try:
 except FileNotFoundError:
     grocery_list = []
 
+#Grocery Categories
+categories = ["Staple","Protein","Produce","Snacks","Drinks","Household"]
+
+#Grocery Stores
+stores = ["PicknPay","Checkers","Spar","Shoprite","Other"]
+
 #Main Loop
 while True:
     #Display CLI menu
     print(f"-----GROCERY DATA TRACKER-----\n1. Add Item\n2. View Items\n3.Total Spend\n4.Exit Tracker")
-    user_choice = int(input("Enter Number: "))
 
+    #Error Handling
+    while True:
+        user_choice = input("Enter Number: ")
+        try:
+            user_choice = int(user_choice)
+            break
+        except ValueError:
+            print("Invalid Entry, Enter a Number!")
+
+    #Match Case
     match user_choice:
         case 1:
-            #Add item loop
+            #Add item
+            #Error Handling
             while True:
-                loop_choice = int(input("Enter 0 to Continue / 1 to Exit: "))
-                if loop_choice != 0:
+                name_input = input("Enter item Name: ")
+                try:
+                    if name_input.strip() == "":
+                        print("Invalid Entry, Please enter a valid item name!")
+                        continue
+                    item_name = name_input.strip()
                     break
+                except ValueError:
+                    print("Invalid Entry, Please enter valid item name!")
+            
+            while True:
+                price_input = input("Enter item Price: ")
+                try:
+                    item_price = float(price_input)
+                    break
+                except ValueError:
+                    print("Invalid Entry, Please enter valid item price!")
 
-                grocery_item = {"Item Name":input("Enter item Name: "),"Price":float(input("Enter item Price: ")),"Quantity":int(input("Enter item Quantity: ")),"Date":date.today().isoformat(),"Category":input("Enter item Category: "),"Store":input("Enter store Name: ")}
-                grocery_list.append(grocery_item)
-                print("Item succesfully added!")
+            while True:
+                quantity_input = input("Enter item Quantity: ")
+                try:
+                    item_quantity = int(quantity_input)
+                    if item_quantity <= 0:
+                        print("Invalid quantity, cannot be less than 1!")
+                        continue
+                    break
+                except ValueError:
+                    print("Invalid Entry, Please enter valid item quantity!")
+
+            print("Categories: \n")
+            for i in range(len(categories)): 
+                print(f"{i+1}.{categories[i]}")
+            
+            while True:
+                category_input = input("Select item Category: ")
+                try:
+                    category_input = int(category_input)
+                    if category_input > 0 and category_input <= len(categories):
+                        category_input = category_input - 1
+                        item_category = categories[category_input]
+                        break
+                    print("Invalid selection, please select from category list!")
+                except ValueError:
+                    print("Invalid Entry, please select category num!")
+
+            print("Stores: \n")
+            for i in range(len(stores)):
+                print(f"{i+1}.{stores[i]}")
+
+            while True:
+                store_input = input("Select item Store: ")
+                try:
+                    store_input = int(store_input)
+                    if  store_input < len(stores) and store_input > 0:
+                        store_input = store_input - 1
+                        item_store = stores[store_input]
+                        break
+                    print("Invalid selection, please select from store list!")
+                except ValueError:
+                    print("Invalid Entry, please select store num!")
+
+            grocery_item = {"Item Name":item_name,"Price":item_price,"Quantity":item_quantity,"Date":date.today().isoformat(),"Category":item_category,"Store":item_store}
+            grocery_list.append(grocery_item)
+            print("Item succesfully added!")
 
             with open("grocery_data.json","w") as file:
                 json.dump(grocery_list, file, indent = 4)
